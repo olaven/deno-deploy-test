af3f13759ec5e28cee25fc8df1b4cb094bee18c2
Olav Sundfør <olav@sundfoer.com>
2021-03-31 22:02:49 +0200

    copy-pasted example from deno.com

--- server.ts
+++ server.ts
@@ -1,13 +1,37 @@
-import { serve } from "https://deno.land/std@0.91.0/http/server.ts";
+function handleRequest(request) {
+  const { pathname } = new URL(request.url);
 
-const server = serve({ hostname: "0.0.0.0", port: 8080 });
-console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
+  // Respond with HTML
+  if (pathname.startsWith("/html")) {
+    const html = `<html>
+      <p><b>Message:</b> Hello from Deno Deploy.</p>
+      </html>`;
 
-for await (const request of server) {
-  let bodyContent = "Your user-agent is:\n\n";
-  bodyContent += request.headers.get("user-agent") || "Unknown";
+    return new Response(html, {
+      headers: {
+        // The "text/html" part implies to the client that the content is HTML
+        // and the "charset=UTF-8" part implies to the client that the content
+        // is encoded using UTF-8.
+        "content-type": "text/html; charset=UTF-8",
+      },
+    });
+  }
 
-  request.respond({ status: 200, body: bodyContent });
+  // Respond with JSON
+  if (pathname.startsWith("/json")) {
+    // Use stringify function to convert javascript object to JSON string.
+    const json = JSON.stringify({
+      message: "Hello from Deno Deploy",
+    });
+
+    return new Response(json, {
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+    });
+  }
 }
 
-
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
